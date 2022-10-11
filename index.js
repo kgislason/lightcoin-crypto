238b33b5422c83dbf13f3161531e2e86188c7842
Kristy Gislason <kristygislason@gmail.com>
2022-10-10 21:13:13 -0700

    refactor account trasnaction classes

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
-let balance = 500.00;
+//let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username,
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (const tr of this.transactions) {
+      balance += tr.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (this.isAllowed()) {
+      // keep track of the time of the transaction
+      this.time = new Date();
+      // Add the transaction to the account
+      this.account.addTransaction(this);
+    } else {
+      return false;
+    }
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
   }
 
 }
 
+class Withdrawal extends Transaction {
 
+  get value() {
+    return -this.amount;
+  }
 
+  isAllowed() {
+    return (this.account.balance > this.amount);
+  }
+
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account("snow-patrol");
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Starting Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+const t1 = new Withdrawal(50.25, myAccount);
+console.log('T1: ', t1.commit());
+
+const t2 = new Deposit(309.99, myAccount);
+t2.commit()
+console.log('T2: ', t2);
+
+const t3 = new Deposit(120.00, myAccount);
+t3.commit()
+console.log('T3: ', t3);
+
+console.log('Ending Balance:', myAccount.balance);
